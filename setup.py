444b142b0e035c13ff0d2bf8c7b0c813b03a4094
Iikka Hauhio <iikka@kaivos.org>
2018-04-02 21:01:12 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+	name='retki',
+	version='0.1',
+	description='Finnish programming language inspired by Inform 7',
+	url='http://github.com/fergusq/retki',
+	author='Iikka Hauhio',
+	author_email='iikka.hauhio@gmail.com',
+	license='GPL',
+	classifiers=[
+		'Programming Language :: Python :: 3'
+	],
+	packages=['retki'],
+	python_requires='>=3',
+	install_requires=['voikko', 'suomilog'],
+	package_data={
+		'retki': ['std.txt'],
+	},
+	entry_points={
+		'console_scripts': [
+			'retki=retki.compiler:main',
+		],
+	},
+)
