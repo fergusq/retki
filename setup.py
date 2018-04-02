f3ecb9adafb7ef192efa5d683047c1771f0118be
Iikka Hauhio <iikka@kaivos.org>
2018-04-03 00:10:06 +0300

    increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='retki',
-	version='0.1',
+	version='0.2',
 	description='Finnish programming language inspired by Inform 7',
 	url='http://github.com/fergusq/retki',
 	author='Iikka Hauhio',
